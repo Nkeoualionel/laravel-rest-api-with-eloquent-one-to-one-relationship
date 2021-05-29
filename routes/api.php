c25a1c2b2de59d8fc845b326d3f07250563fb260
Nkeoualionel <mbembasachat9@gmail.com>
2021-05-29 07:14:04 +0200

    debug api route error

--- routes/api.php
+++ routes/api.php
@@ -24,11 +24,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('get-roles', [RoleController::class, 'index']);
 Route::get('show-role', [RoleController::class, 'show']);
 Route::post('add-role', [RoleController::class, 'store']);
-Route::update('update-role', [RoleController::class, 'update']);
+Route::put('update-role', [RoleController::class, 'update']);
 Route::delete('delete-role', [RoleController::class, 'delete']);
 
 Route::get('get-personnels', [Personnelcontroller::class, 'index']);
 Route::get('show-personnel', [Personnelcontroller::class, 'show']);
 Route::post('add-personnel', [Personnelcontroller::class, 'store']);
-Route::update('update-personnel', [Personnelcontroller::class, 'update']);
+Route::put('update-personnel', [Personnelcontroller::class, 'update']);
 Route::delete('delete-personnel', [Personnelcontroller::class, 'delete']);
